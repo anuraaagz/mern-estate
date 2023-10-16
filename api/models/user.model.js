9472f96e32063abe94a52f9765a0025a57354f50
anuraaagz <mathuranurag1807@gmail.com>
2023-10-17 00:16:36 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import { MongoGridFSChunkError } from "mongodb";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } //to record time pf user creation and time of update of the user
+);
+
+const User = mongoose.model('User', userSchema); 
+
+export default User;
