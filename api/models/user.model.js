e09267af0bb9c6bf8a13fa0a6391927e3a249e80
anuraaagz <mathuranurag1807@gmail.com>
2023-10-22 17:15:37 +0530

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,11 +16,16 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-    }
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=6&m=1223671392&s=612x612&w=0&h=NGxdexflb9EyQchqjQP0m6wYucJBYLfu46KCLNMHZYM=",
+    },
   },
   { timestamps: true } //to record time pf user creation and time of update of the user
 );
 
-const User = mongoose.model('User', userSchema); 
+const User = mongoose.model("User", userSchema);
 
 export default User;
