9f91310fe7e104cc82830328e5be7c79e5423f59
anuraaagz <mathuranurag1807@gmail.com>
2023-10-24 14:13:51 +0530

    update header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=6&m=1223671392&s=612x612&w=0&h=NGxdexflb9EyQchqjQP0m6wYucJBYLfu46KCLNMHZYM=",
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
   },
   { timestamps: true } //to record time pf user creation and time of update of the user
