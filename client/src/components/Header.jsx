20724eece45c228608dad9dfab395cdf6c0173d4
anuraaagz <mathuranurag1807@gmail.com>
2023-10-24 15:03:01 +0530

    complete profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -37,7 +37,7 @@ export default function Header() {
           </Link>
           <Link to="/profile">
             {currentUser ? (
-              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
+              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} />
             ) : (
               <li className="text-slate-700 hover:underline">Sign In</li>
             )}
